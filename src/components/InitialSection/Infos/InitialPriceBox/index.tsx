bbf96bb0e3cb1e702584ba45ff33f7768ff7379c
Alessandro Gomes <alllessandrogomes@gmail.com>
2025-02-22 17:51:46 -0300

    :pencil: altera o preço da bateria na InitialSection.

--- src/components/InitialSection/Infos/InitialPriceBox/index.tsx
+++ src/components/InitialSection/Infos/InitialPriceBox/index.tsx
@@ -4,7 +4,7 @@ import styles from "./styles.module.scss"
 export default function InitialPriceBox() {
     return (
         <div className={styles.priceBox}>
-            <h3>Baterias a partir de: <br /><span className={styles.yellowSpan}>R$ 209,99</span></h3>
+            <h3>Baterias a partir de: <br /><span className={styles.yellowSpan}>R$ 239,99</span></h3>
             <div className={styles.batterysBox}>
                 <img className={styles.moura} src="/images/moura-battery-1.webp" alt="Imagem Bateria Moura" title="Bateria Moura" />
                 <img className={styles.kondor} src="/images/kondor-battery-1.webp" alt="Imagem Bateria Kondor" title="Bateria Kondor" />
