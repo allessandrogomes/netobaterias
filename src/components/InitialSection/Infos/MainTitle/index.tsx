a64d440251eb49297c3d89efc74d8ca8058ae0e7
Alessandro <alllessandrogomes@gmail.com>
2025-02-26 08:29:24 -0300

    :recycle:refactor: altera o h1 para melhorar SEO.

--- src/components/InitialSection/Infos/MainTitle/index.tsx
+++ src/components/InitialSection/Infos/MainTitle/index.tsx
@@ -3,7 +3,7 @@ import styles from "./styles.module.scss"
 export default function MainTitle() {
     return (
         <h1 className={styles.title}>
-            Sua Bateria na Hora Certa, Pelo Melhor Preço na <span className={styles.blueSpan}>Neto</span> <span className={styles.yellowSpan}>Baterias</span>.
+            Sua Bateria em Juazeiro e Petrolina, Pelo Melhor Preço na <span className={styles.blueSpan}>Neto</span> <span className={styles.yellowSpan}>Baterias</span>.
         </h1>
     )
 }
